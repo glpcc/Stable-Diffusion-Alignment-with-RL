d057521b7bb020979800474c079d9242834ba6ef
Gonzalo Lope <gonzalolopecc@gmail.com>
2025-04-08 18:04:26 +0200

    reorganized and structured the train script

---
+++ training/clip.py
@@ -0,0 +1,54 @@
+from transformers import CLIPModel, CLIPProcessor
+import torch
+
+
+
+def get_clip_image_embedding(images):
+    """
+    Get the CLIP image embedding for a given image.
+
+    Args:
+        image_path (str): Path to the image file.
+
+    Returns:
+        torch.Tensor: The CLIP image embedding.
+    """
+    # Load the CLIP model and processor
+    clip = CLIPModel.from_pretrained("openai/clip-vit-large-patch14")
+    processor = CLIPProcessor.from_pretrained("openai/clip-vit-large-patch14")
+
+    # Preprocess the image
+    inputs = processor(images=images, return_tensors="pt")
+
+    # Get the image features from the model
+    image_features = clip.get_image_features(**inputs)
+
+    # Normalize the features
+    image_features = image_features / torch.linalg.vector_norm(image_features, dim=-1, keepdim=True)
+
+    return image_features
+
+def get_clip_text_embedding(text: str):
+    """
+    Get the CLIP text embedding for a given text.
+
+    Args:
+        text (str): The input text.
+
+    Returns:
+        torch.Tensor: The CLIP text embedding.
+    """
+    # Load the CLIP model and processor
+    clip = CLIPModel.from_pretrained("openai/clip-vit-large-patch14")
+    processor = CLIPProcessor.from_pretrained("openai/clip-vit-large-patch14")
+
+    # Preprocess the text
+    inputs = processor(text=text, return_tensors="pt")
+
+    # Get the text features from the model
+    text_features = clip.get_text_features(**inputs)
+
+    # Normalize the features
+    text_features = text_features / torch.linalg.vector_norm(text_features, dim=-1, keepdim=True)
+
+    return text_features
---
+++ training/train.py
@@ -0,0 +1,145 @@
+import os
+from dataclasses import dataclass, field
+import pathlib
+import numpy as np
+import torch
+import torch.nn as nn
+from huggingface_hub import hf_hub_download
+from huggingface_hub.utils import EntryNotFoundError
+from transformers import CLIPModel, CLIPProcessor, HfArgumentParser, is_torch_npu_available, is_torch_xpu_available
+from clip import get_clip_image_embedding, get_clip_text_embedding
+import yaml
+from PIL import Image
+import json
+from trl import DDPOConfig, DDPOTrainer, DefaultDDPOStableDiffusionPipeline
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+
+path = pathlib.Path(__file__).parent.resolve()
+
+
+class ImageSimilarityScorer():
+    """
+    This model attempts to make the generated images to be as similar as possible to the reference image
+    """
+
+    def __init__(self,reference_image, *, dtype):
+        super().__init__()
+        self.reference_image_features = get_clip_image_embedding(reference_image)
+        self.dtype = dtype
+
+    @torch.no_grad()
+    def __call__(self, images):
+        print(f"Shape of images: {images.shape}")
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        inputs = {k: v.to(self.dtype).to(device) for k, v in images.items()}
+        embed = get_clip_image_embedding(inputs)
+        return torch.nn.functional.cosine_similarity(embed, self.reference_image_features, dim=-1) *10
+
+
+class TextSimilarityScorer():
+    """
+    This model attempts to make the generated images to be as similar as possible to the clip text prompt
+
+    """
+
+    def __init__(self,reference_text, *, dtype):
+        super().__init__()
+        self.reference_features = get_clip_text_embedding(reference_text)
+        self.dtype = dtype
+
+    @torch.no_grad()
+    def __call__(self, images):
+        print(f"Shape of images: {images.shape}")
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        inputs = {k: v.to(self.dtype).to(device) for k, v in images.items()}
+        embed = get_clip_image_embedding(inputs)
+        return torch.nn.functional.cosine_similarity(embed, self.reference_features, dim=-1)*10 
+
+
+
+
+def image_outputs_logger(image_data, global_step, accelerate_logger):
+    result = {}
+    for data in image_data[-4:]:
+        images, prompts, _, rewards, _ = data
+
+        for i, image in enumerate(images):
+            prompt = prompts[i]
+            reward = rewards[i].item()
+            result[f"{prompt}__{reward:.5f}"] = image.unsqueeze(0).float()
+
+    accelerate_logger.log_images(
+        result,
+        step=global_step,
+    )
+
+def load_config(config_path,run_name):
+    with open(config_path, "r") as f:
+        config = yaml.safe_load(f)
+    config["ddpo_config"]["run_name"] = run_name
+    # Create the folder for the run if it doesn't exist
+    run_folder = path / "runs" / run_name
+    os.makedirs(run_folder, exist_ok=True)
+    # Set the logging and project directories in the config
+    config["ddpo_config"]["project_kwargs"]["logging_dir"] = str(run_folder / "logs")
+    config["ddpo_config"]["project_kwargs"]["project_dir"] = str(run_folder / "save")
+
+    # copy the config file to the run folder
+    config_file_path = run_folder / "config.yaml"
+    with open(config_file_path, "w") as f:
+        yaml.dump(config, f)
+    
+
+    return config
+
+def load_pipeline(config):
+    pipeline = DefaultDDPOStableDiffusionPipeline(
+        pretrained_model_name=config["script_config"]["pretrained_model_name"],
+        pretrained_model_revision =config["script_config"]["pretrained_model_revision"],
+        use_lora=config["script_config"]["use_lora"]
+    )
+    if config["resume_from"] != "":
+        pipeline.load_lora_weights(config["resume_from"],weight_name="pytorch_lora_weights.safetensors")
+    
+    return pipeline
+
+def load_scorer(config) -> function:
+    match config["script_config"]["scorer"]:
+        case "text":
+            reference_text = config["reference_text"]
+            scorer = TextSimilarityScorer(reference_text=reference_text,dtype=torch.float32)
+        case "image":
+            reference_image = Image.open(config["reference_image"])
+            scorer = ImageSimilarityScorer(reference_image=reference_image,dtype=torch.float32)
+        case "custom":
+            raise NotImplementedError("Custom scorer not implemented.")
+        case _:
+            raise ValueError("Invalid scorer type. Choose 'text' or 'image'.")
+    
+    def scorer_fn(images, prompts, model):
+        scores = scorer(images)
+        print(scores)
+        return scores, {}
+    
+    return scorer_fn
+
+def train(run_name: str):
+    config = load_config("train_config.yaml",run_name)
+    parser = HfArgumentParser(DDPOConfig)
+    ddpo_config, = parser.parse_dict(config["ddpo_config"])
+    pipeline = load_pipeline(config)
+    scorer_fn = load_scorer(config)
+    trainer = DDPOTrainer(
+        ddpo_config,
+        reward_function=scorer_fn,
+        prompt_function=lambda : (np.random.choice(config["prompts"]), {}),
+        sd_pipeline=pipeline,
+        image_samples_hook=image_outputs_logger,
+    )
+
+if __name__ == "__main__":
+    run_name = input("Enter run name: ")
+    train(run_name)
